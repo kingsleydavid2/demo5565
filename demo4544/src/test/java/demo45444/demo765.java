4dac9e08094664b2ba8072d5f44ae11088e96fd4
Mohan S <kingsleydavid.s@thestreet.com>
2022-05-10 00:38:50 +0530

    first commit to github

---
+++ demo4544/src/test/java/demo45444/demo765.java
@@ -0,0 +1,12 @@
+package demo45444;
+
+public class demo765 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi sasikumar");
+
+	}
+
+}
